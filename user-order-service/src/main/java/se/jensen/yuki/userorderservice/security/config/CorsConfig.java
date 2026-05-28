3dd81c928b009a17761a96e7e4d15f9b910d4bbc
YukiJanse <yuki.janse@gmail.com>
2026-05-28 11:21:45 +0200

    Add log for debug

--- user-order-service/src/main/java/se/jensen/yuki/userorderservice/security/config/CorsConfig.java
+++ user-order-service/src/main/java/se/jensen/yuki/userorderservice/security/config/CorsConfig.java
@@ -1,27 +1,33 @@
 package se.jensen.yuki.userorderservice.security.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Configuration class to set up CORS (Cross-Origin Resource Sharing) settings.
  */
+@Slf4j
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        // Allow all origins
-        config.setAllowedOrigins(List.of(
+        List<String> allowedOrigins = List.of(
                 "http://localhost:5173"
-        )); // Note: In production, specify allowed origins for better security
+        );
+
+        // Allow all origins
+        config.setAllowedOrigins(allowedOrigins); // Note: In production, specify allowed origins for better security
         // Allow specific HTTP methods
+        log.debug("Cors allowed origins: {}", allowedOrigins);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         // Allow specific headers
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
