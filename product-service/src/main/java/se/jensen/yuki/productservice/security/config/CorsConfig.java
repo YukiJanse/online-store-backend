a1048bf471d1222b5b4c7880fb023156a38dd65c
YukiJanse <yuki.janse@gmail.com>
2026-06-05 15:18:49 +0200

    Modify allowed origins

--- product-service/src/main/java/se/jensen/yuki/productservice/security/config/CorsConfig.java
+++ product-service/src/main/java/se/jensen/yuki/productservice/security/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
         List<String> allowedOrigins = List.of(
                 "http://localhost:5173",
                 "http://94.255.243.225",
-                "https://yukisonlinestore.netlify.app/"
+                "https://yukisonlinestore.netlify.app"
         );
 
         // Allow all origins
